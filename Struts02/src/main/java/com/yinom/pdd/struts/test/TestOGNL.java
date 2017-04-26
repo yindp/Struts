a6b959bb8659076989373c0e2e94402993c92161
yindp <378714297@qq.com>
2017-04-26 20:09:20 +0800

    Struts OGNL get List value.

--- Struts02/src/main/java/com/yinom/pdd/struts/test/TestOGNL.java
+++ Struts02/src/main/java/com/yinom/pdd/struts/test/TestOGNL.java
@@ -6,7 +6,9 @@ import ognl.Ognl;
 import ognl.OgnlException;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -48,6 +50,31 @@ public class TestOGNL {
             //OGNL is a super Context and the root key is "root". So we can get value from "#root"
             System.out.println(Ognl.getValue("#root.salary", ctx, emp));
 
+        } catch (OgnlException e) {
+            e.printStackTrace();
+        } finally {
+        }
+    }
+
+    @Test
+    public void test03() {
+        Employee emp1 = new Employee("121", "admin", "123", "administrator", 1000);
+        Employee emp2 = new Employee("121", "coco", "123", "it", 10100);
+        Employee emp3 = new Employee("121", "Eri", "123", "user", 10030);
+        Department dept = new Department("1234", "Research");
+        emp1.setDepartment(dept);
+        emp2.setDepartment(dept);
+        emp3.setDepartment(dept);
+        List<Employee> emps = new ArrayList<Employee>();
+        emps.add(emp1);
+        emps.add(emp2);
+        emps.add(emp3);
+
+        try {
+            System.out.println(Ognl.getValue("#root[0].nickname", emps));
+            for (int i = 0; i < emps.size(); i++) {
+                System.out.println(Ognl.getValue("#root[" + i + "]", emps));
+            }
         } catch (OgnlException e) {
             e.printStackTrace();
         } finally {
